5728ce4f865cd289fb20080cf0b947c0739c3f7f
jan.sch.hansen@gmail.com <jan.sch.hansen@gmail.com@bbbbd226-e4a7-7667-7660-4d0d2b0f8313>
2011-10-24 17:33:19 +0000

    Backup

--- trunk/Code/UserDrivenSignLanguageDictionary/src/java/dk/jsh/itdiplom/userdrivensignlanguagedictionary/wicket/about/About.java
+++ trunk/Code/UserDrivenSignLanguageDictionary/src/java/dk/jsh/itdiplom/userdrivensignlanguagedictionary/wicket/about/About.java
@@ -2,6 +2,14 @@ package dk.jsh.itdiplom.userdrivensignlanguagedictionary.wicket.about;
 
 import dk.jsh.itdiplom.userdrivensignlanguagedictionary.wicket.BasePage;
 import dk.jsh.itdiplom.userdrivensignlanguagedictionary.wicket.homepage.MenuBorder;
+import dk.jsh.itdiplom.userdrivensignlanguagedictionary.wicket.html5.Html5Video;
+import dk.jsh.itdiplom.userdrivensignlanguagedictionary.wicket.html5.VideoSource;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.markup.html.border.Border.BorderBodyContainer;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
 
 /**
  * About page.
@@ -10,7 +18,31 @@ import dk.jsh.itdiplom.userdrivensignlanguagedictionary.wicket.homepage.MenuBord
  */
 public final class About extends BasePage {
 
+    
     public About() {
-        add(new MenuBorder("mainNavigation"));
+        MenuBorder menuBorder = new MenuBorder("mainNavigation");
+        add(menuBorder);
+        BorderBodyContainer borderBodyContainer = menuBorder.getBodyContainer();
+        
+        
+        
+        final List<VideoSource> videoSources = new ArrayList<VideoSource>();
+        videoSources.add(new VideoSource("movie.ogg", 
+                 VideoSource.VideoType.OGG));
+        
+        IModel<List<VideoSource>> videoSourceList = 
+                new AbstractReadOnlyModel<List<VideoSource>>() {
+            public List<VideoSource> getObject() {
+                return videoSources;
+            }
+        };
+        Html5Video html5Video = new Html5Video("aboutVideo", videoSourceList);
+        
+        borderBodyContainer.add(html5Video);
+
+        
+        
+        
+        
     }
 }

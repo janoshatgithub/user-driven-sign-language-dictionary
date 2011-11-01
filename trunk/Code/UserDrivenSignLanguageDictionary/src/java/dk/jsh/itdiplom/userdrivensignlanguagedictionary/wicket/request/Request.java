3e3cc3e87496de35bff93370cd140e0f2f5fff81
jan.sch.hansen@gmail.com <jan.sch.hansen@gmail.com@bbbbd226-e4a7-7667-7660-4d0d2b0f8313>
2011-11-01 19:44:04 +0000

    Word, WordGroup and WordGroupWordRelation entities added.

--- trunk/Code/UserDrivenSignLanguageDictionary/src/java/dk/jsh/itdiplom/userdrivensignlanguagedictionary/wicket/request/Request.java
+++ trunk/Code/UserDrivenSignLanguageDictionary/src/java/dk/jsh/itdiplom/userdrivensignlanguagedictionary/wicket/request/Request.java
@@ -2,6 +2,12 @@ package dk.jsh.itdiplom.userdrivensignlanguagedictionary.wicket.request;
 
 import dk.jsh.itdiplom.userdrivensignlanguagedictionary.wicket.BasePage;
 import dk.jsh.itdiplom.userdrivensignlanguagedictionary.wicket.homepage.MenuBorder;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.wicket.markup.html.border.Border.BorderBodyContainer;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RadioChoice;
+import org.apache.wicket.model.PropertyModel;
 
 /**
  * Request page.
@@ -9,8 +15,29 @@ import dk.jsh.itdiplom.userdrivensignlanguagedictionary.wicket.homepage.MenuBord
  * @author Jan S. Hansen
  */
 public final class Request extends BasePage {
+    //choices in radio button
+    private static final List<String> radioChoices = Arrays
+			.asList(new String[] {"Mine", "Alle"});
+    private String selected = "Mine";
 
     public Request() {
-        add(new MenuBorder("mainNavigation"));
+        MenuBorder menuBorder = new MenuBorder("mainNavigation"); 
+        add(menuBorder);
+        BorderBodyContainer borderBodyContainer = menuBorder.getBodyContainer();
+        Form form = new Form("form") {
+            
+            
+        };
+        borderBodyContainer.add(form);
+        
+        RadioChoice<String> userOrAll = new RadioChoice<String>(
+            "showAllRequest", new PropertyModel<String>(this, "selected"), 
+                radioChoices);
+        userOrAll.setSuffix("");
+        form.add(userOrAll);
+        
+        
+        
+        
     }
 }

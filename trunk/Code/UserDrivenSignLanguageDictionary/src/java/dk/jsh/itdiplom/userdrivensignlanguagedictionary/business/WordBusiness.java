c1475fced6ed019c8c8b5d5e639c690dcba3455a
jan.sch.hansen@gmail.com <jan.sch.hansen@gmail.com@bbbbd226-e4a7-7667-7660-4d0d2b0f8313>
2011-11-03 18:02:39 +0000

    Backup

--- trunk/Code/UserDrivenSignLanguageDictionary/src/java/dk/jsh/itdiplom/userdrivensignlanguagedictionary/business/WordBusiness.java
+++ trunk/Code/UserDrivenSignLanguageDictionary/src/java/dk/jsh/itdiplom/userdrivensignlanguagedictionary/business/WordBusiness.java
@@ -26,5 +26,30 @@ public class WordBusiness {
         tx.commit();
         session.close();
     }    
+    
+    /**
+     * Test if a word exists.
+     * 
+     * @param word word to test
+     * @return true is the word exists 
+     */
+    public static boolean isWordInUse(String word) {
+        boolean inUse = false;
 
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        String hql = "select word "
+                + "from dk.jsh.itdiplom.userdrivensignlanguagedictionary.entity."
+                + "Word word "
+                + "where word.word = :word";
+        Query query = session.createQuery(hql);
+        query.setString("word", word);
+        if (query.list().isEmpty()) {
+            inUse = false;
+        }
+        else {
+            inUse = true;
+        };
+        session.close();
+        return inUse;
+    }
 }

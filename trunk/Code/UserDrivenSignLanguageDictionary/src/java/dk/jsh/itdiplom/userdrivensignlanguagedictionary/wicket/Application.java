98edd86f0659ce445bdd5bb63722afecf59ef5c7
jan.sch.hansen@gmail.com <jan.sch.hansen@gmail.com@bbbbd226-e4a7-7667-7660-4d0d2b0f8313>
2011-11-12 20:52:32 +0000

    Upload page added

--- trunk/Code/UserDrivenSignLanguageDictionary/src/java/dk/jsh/itdiplom/userdrivensignlanguagedictionary/wicket/Application.java
+++ trunk/Code/UserDrivenSignLanguageDictionary/src/java/dk/jsh/itdiplom/userdrivensignlanguagedictionary/wicket/Application.java
@@ -1,6 +1,8 @@
 package dk.jsh.itdiplom.userdrivensignlanguagedictionary.wicket;
 
 import dk.jsh.itdiplom.userdrivensignlanguagedictionary.wicket.homepage.HomePage;
+import java.util.logging.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.wicket.Request;
 import org.apache.wicket.Response;
 import org.apache.wicket.Session;
@@ -13,7 +15,12 @@ import org.apache.wicket.protocol.http.WebApplication;
  * @author Jan S. Hansen
  */
 public class Application extends WebApplication {
+    static final Logger logger = Logger.getLogger(Application.class.getName());
+    
     public Application() {
+        //PropertyConfigurator.configure("WEF-INF/classes/log4j.properties");
+        System.setProperty("log4j.configuration", "./WEB-INF/classes/log4j.properties");
+        logger.info("Application started.");
     }
 
     @Override
